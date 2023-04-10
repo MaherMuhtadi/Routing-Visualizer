5007f961cc492f9d22761d03d59082f56402bb7b
Maher Muhtadi <m.muhtadi2002@gmail.com>
2023-04-10 16:54:33 -0400

    Implemented nodes layout

--- renderer.js
+++ renderer.js
@@ -2,11 +2,22 @@ var cy = cytoscape({
     container: document.getElementById("cy"), // container to render in
 });
 
+// test nodes
 cy.add([
-    { group: 'nodes', data: { id: 'n0' }, position: { x: 1000, y: 100 } },
-    { group: 'nodes', data: { id: 'n1' }, position: { x: 200, y: 200 } },
-    { group: 'edges', data: { id: 'e0', source: 'n0', target: 'n1' } }
+    { group: 'nodes', data: { id: 'n0' }},
+    { group: 'nodes', data: { id: 'n1' }},
+    { group: 'nodes', data: { id: 'n2' }},
+    { group: 'nodes', data: { id: 'n3' }},
+    { group: 'nodes', data: { id: 'n4' }},
+    { group: 'nodes', data: { id: 'n5' }},
+    { group: 'nodes', data: { id: 'n6' }},
+    { group: 'edges', data: { id: 'e0', source: 'n0', target: 'n1' } },
+    { group: 'edges', data: { id: 'e1', source: 'n3', target: 'n2' } },
+    { group: 'edges', data: { id: 'e2', source: 'n6', target: 'n0' } },
+    { group: 'edges', data: { id: 'e3', source: 'n4', target: 'n5' } }
 ]);
 
-cy.center();
-cy.fit();
+cy.layout({
+    name: "concentric",
+    minNodeSpacing: 50
+}).run();

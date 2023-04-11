3ed41fa9c330a410be0e8f5fae2b0500e5ed635e
Maher Muhtadi <m.muhtadi2002@gmail.com>
2023-04-10 20:39:22 -0400

    Implemented user input for rendering graph

--- renderer.js
+++ renderer.js
@@ -1,24 +1,50 @@
 var net = cytoscape({
     container: document.getElementById("canvas"), // container to render in
-    style: [{
-          selector: 'node',
-          style: {
-            'label': 'data(label)', // uses the "label" property from node's data property as the label
-          }
-        }],
+    style: [
+        {
+            selector: 'node',
+            style: {
+                'label': 'data(label)', // uses the "label" property value from node's data property
+                'text-valign': 'center', // sets vertical alignment of label
+                'text-halign': 'center' // sets horizontal alignment of label
+            }
+        },
+        {
+            selector: 'edge',
+            style: {
+                'label': 'data(weight)', // uses the "weight" property value from edge's data property as the label
+                'text-margin-x': 10, // sets margin for label along the x-axis
+                'text-margin-y': -10 // sets margin for label along the y-axis
+            }
+        }
+    ],
 });
 
-var nodes = ['n1', 'n2', 'n3', 'n4', 'n5', 'n6'];
-var edges = [['n1', 'n2', 5], ['n1', 'n4', 6], ['n1', 'n3', 3], ['n2', 'n4', 4], ['n2', 'n5', 2], ['n2', 'n6', 2], ['n3', 'n4', 2], ['n3', 'n6', 8], ['n5', 'n6', 7]];
-g = new graph(nodes, edges);
+var g = new graph();
 
-for (let node of g.nodes) {
-    net.add({group: "nodes", data: {id: node, label: node}});
-}
-for (let edge of g.edges) {
-    net.add({group: "edges", data: {id: edge[0]+edge[1], source: edge[0], target: edge[1]}});
+/**
+ * Takes the value from node input field and adds the node to the graph.
+ * If successful, renders the node on the canvas.
+ */
+function renderNode() {
+    let node = document.getElementById("node").value;
+    
+    if (g.addNode(node)) {
+        net.add({group: "nodes", data: {id: node, label: node}});
+        net.layout({name: "grid"}).run();
+    }
 }
 
-net.layout({
-    name: "grid",
-}).run();
+/**
+ * Takes the values from edge input fields and adds the edge to the graph.
+ * If successful, renders the edge on the canvas.
+ */
+function renderEdge() {
+    let node_x = document.getElementById("node_x").value;
+    let node_y = document.getElementById("node_y").value;
+    let weight = Number(document.getElementById("weight").value);
+    
+    if (g.addEdge(node_x, node_y, weight)) {
+        net.add({group: "edges", data: {id: node_x+"_"+node_y, weight: weight, source: node_x, target: node_y}});
+    }
+}
